458a9e00ff28a0db985563908e253724a41d25e9
Robinson Arysseril <72635727+death7654@users.noreply.github.com>
2026-07-17 18:26:15 +0530

    cargo fmt

--- src/Emulator/bus/mod.rs
+++ src/Emulator/bus/mod.rs
@@ -1,41 +1,28 @@
 use crate::Emulator::memory;
 
-pub struct bus
-{
-  ram: memory::memory
+pub struct Bus {
+    ram: memory::memory,
 }
 
-impl bus
-{
-  pub fn new() -> Self
-  {
-    let memory = memory::memory::new();
-    Self
-    {
-      ram: memory
+impl Bus {
+    pub fn new() -> Self {
+        let memory = memory::memory::new();
+        Self { ram: memory }
     }
-  }
 
-  pub fn read(self, address: u16) -> u8
-  {
-    match address
-    {
-      0x0000..0x1FFF => {
-        self.ram.read(address)
-      }
-      _ => 0
+    pub fn read(&self, address: u16) -> u8 {
+        match address {
+            0x0000..0x1FFF => self.ram.read(address),
+            _ => 0,
+        }
     }
-  }
 
-  pub fn write(&mut self, address: u16, data: u8)
-  {
-    match address
-    {
-      0x0000..0x1FFF =>
-      {
-        self.ram.write(address, data);
-      },
-      _ => {}
+    pub fn write(&mut self, address: u16, data: u8) {
+        match address {
+            0x0000..0x1FFF => {
+                self.ram.write(address, data);
+            }
+            _ => {}
+        }
     }
-  }
 }
