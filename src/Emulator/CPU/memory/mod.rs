934804fb7b42b4a5c34f08f5d59edad1851f0d8f
Robinson Arysseril <72635727+death7654@users.noreply.github.com>
2026-07-17 07:07:27 +0530

    implement CPU memory

---
+++ src/Emulator/CPU/memory/mod.rs
@@ -0,0 +1,44 @@
+
+
+pub struct memory
+{
+  ram: [u8; 2048]
+}
+
+impl memory
+{
+  pub fn new() -> Self
+  {
+    Self
+    {
+      ram: [0; 2048]
+    } 
+  }
+
+  pub fn read(&self, address: u16) -> u8
+  {
+    // the cpu memory is mirrored 3 times
+    match address
+    {
+    0x0000..=0x1FFF => {
+            let physical_index = (address & 0x07FF) as usize;
+            self.ram[physical_index]
+        }
+        _ => {
+            return 0;
+        }
+    }
+  }
+
+  pub fn write(&self, address: u16, data: u8)
+  {
+    match address
+    {
+      0x0000..=0x1FFF => {
+            let physical_index = (address & 0x07FF) as usize;
+            self.ram[physical_index] = data;
+        }
+        _ => {}
+    }
+  }
+}
